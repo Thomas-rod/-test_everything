b612f1ee12f67cfeb8317233ad15cd23ba0e882a
thomas rodier <rodiert17@gmail.com>
2020-06-12 16:45:25 +0200

    update algolia now it"s working

--- app/javascript/components/algoliasearch.js
+++ app/javascript/components/algoliasearch.js
@@ -6,8 +6,60 @@ const algoliaSearch = () => {
     'd6fb43e818fa0939aef7f26581ba699e'
   );
 
+
+
+  const autocomplete = instantsearch.connectors.connectAutocomplete(
+    ({ indices, refine, widgetParams }, isFirstRendering) => {
+      const { container, onSelectChange } = widgetParams;
+
+      if (isFirstRendering) {
+        container.html('<select id="ais-autocomplete"></select>');
+
+        container.find('select').selectize({
+          options: [],
+          valueField: 'name',
+          labelField: 'name',
+          searchField: 'name',
+          highlight: false,
+          onType: refine,
+          onChange(value) {
+            // We call the provided callback each time a value is selected
+            onSelectChange(value);
+            refine(value);
+          }
+        });
+
+        return;
+      }
+
+      const [select] = container.find('select');
+
+      indices.forEach(index => {
+        select.selectize.clearOptions();
+        index.results.hits.forEach(hit => select.selectize.addOption(hit));
+        select.selectize.refreshOptions(select.selectize.isOpen);
+      });
+    }
+  );
+
+  const suggestions = instantsearch({
+    indexName: 'Character',
+    searchClient,
+  });
+
+  suggestions.addWidgets([
+    autocomplete({
+      container: $('#autocomplete'),
+      onSelectChange(value) {
+       // Now we can trigger the search on our main instance
+       // each time the value inside the dropdown is selected
+       search.helper.setQuery(value).search();
+      },
+    })
+  ]);
+
   const search = instantsearch({
-    indexName: 'demo_ecommerce',
+    indexName: 'Character',
     searchClient,
   });
 
@@ -21,18 +73,18 @@ const algoliaSearch = () => {
       showLoadingIndicator: true
     }),
     instantsearch.widgets.hits({
-      container: document.querySelector('#hits'),
+      container: '#hits',
       templates: {
-      empty: "No results.",
-      item: `
-        <div class="hit-name">
-          {{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}
-        </div>
-      `,
-      }
+        item: `
+          <div>
+            <header class="hit-name">
+              {{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}
+            </header>
+          </div>
+        `,
+      },
     })
   ]);
-
   search.start();
 }
 
